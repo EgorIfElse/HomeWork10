a8ae7e78270b2b996ffcd472e7b35ddd1ecb5c99
user <user@WIN-PRBG4ODTMQN>
2024-11-11 20:26:26 +0300

    Добавьте файлы проекта.

---
+++ HomeWork10/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    internal class Angle
+    {
+        public bool itscorrect = true;
+        private int gradus;
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                if (value >= 0)
+                {
+                    gradus = value % 180;
+                }
+                else
+                {
+                    gradus = 0;
+                    itscorrect = false;
+                }
+
+            }
+        }
+        private int minutes;
+        public int Minutes
+        {
+            get { return minutes; }
+            set
+            {
+                if (value >= 0)
+                {
+                    Gradus = value / 60;
+                    minutes = value % 60;
+                }
+                else
+                {
+                    minutes = 0;
+                    itscorrect = false;
+                }
+
+            }
+        }
+        private int seconds;
+        public int Seconds
+        {
+            get { return seconds; }
+            set
+            {
+                if (value >= 0)
+                {
+                    Minutes = value / 60;
+                    seconds = value % 60;
+                }
+                else
+                {
+                    seconds = 0;
+                    itscorrect = false;
+                }
+
+            }
+        }
+
+        public Angle(int gradus, int minutes, int seconds)
+        {
+            Gradus = gradus;
+            Minutes = minutes;
+            Seconds = seconds;
+
+        }
+
+        public double ToRadians()
+        {
+            return  (gradus + (minutes / 60.0) + (seconds / 3600.0))* Math.PI/180;
+        }
+    }
+}
+    
---
+++ HomeWork10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeWork10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите значения");
+                angle = new Angle(' ', ' ', ' ');
+                angle.Gradus = Convert.ToInt32(Console.ReadLine());
+                angle.Minutes = Convert.ToInt32(Console.ReadLine());
+                angle.Seconds = Convert.ToInt32(Console.ReadLine());
+            }
+            while (angle.itscorrect == false);
+            double toRadins = angle.ToRadians();
+            Console.WriteLine(toRadins);
+        }
+    }
+}
